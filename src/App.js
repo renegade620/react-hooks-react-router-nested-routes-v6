1c9cb7e216b0708074f72a0608599e9a4c082906
Frankline Were <franklinegift@gmail.com>
2024-08-08 15:24:24 +0300

    add outlet component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <header>
         <NavBar />
       </header>
+      <Outlet />
     </>
   );
 };
