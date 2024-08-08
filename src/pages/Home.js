dfc53afaecc6e1a91c7b89b965be21a7a3a251bc
Frankline Were <franklinegift@gmail.com>
2024-08-08 15:54:09 +0300

    passed data via useOuutletContext

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,16 @@
-import { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 import UserCard from "../components/UserCard";
 
 function Home() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
-  });
+  const users = useOutletContext();
+  const userList = users.map(user => <UserCard key={user.id} user={user}/>);
 
   return (
-    <>
       <main>
         <h1>Home!</h1>
         <Outlet />
         {userList}
       </main>
-    </>
   );
 };
 
